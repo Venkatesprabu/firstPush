147de2e040b7343e16e7308ff3129663d81faaa5
venkatesprabu12 <venkatesprabu17@gmail.com>
2022-05-10 18:04:12 +0530

    first push

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,38 @@
+package org.phone;
+
+public class PhoneInfo {
+	public static void main(String[] arg) {
+		PhoneInfo d = new PhoneInfo();
+		d.phoneName();
+		d.phoneMieiNumber();
+		d.camera();
+		d.storage();
+		d.osName();
+	}
+
+	private void phoneName() {
+
+		System.out.println("phonenumber");
+
+	}
+
+	private void phoneMieiNumber() {
+
+		System.out.println("phoneMieiNumber");
+	}
+
+	private void camera() {
+
+		System.out.println("Camea");
+	}
+
+	private void storage() {
+
+		System.out.println("Storage");
+	}
+
+	private void osName() {
+
+		System.out.println("OsName");
+	}
+}
